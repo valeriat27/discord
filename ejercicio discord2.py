76623005f085872917eb1dad95e35f24aee7afb5
valeria <valeriatobon200614@gmail.com>
2025-04-25 07:48:22 -0500

    ejercicios_practica

---
+++ ejercicio discord2.py
@@ -0,0 +1,12 @@
+print("formatear_nombre")
+nombre = input("cual es tu nombre: ")
+segundo_nombre = input("cual es tu segundo_nombre: ")
+apellido = input("cual es tu apellido: ")
+
+formatear_nombre = (nombre,segundo_nombre,apellido)
+print  (formatear_nombre)
+
+
+
+
+
---
+++ ejercicio discord3.py
@@ -0,0 +1,7 @@
+print("convertir_a_usd")
+salario = int(input("cual es tu salario: "))
+tasa_de_cambio = 0.00023
+salario_dolar = salario * tasa_de_cambio
+print(f"su tasa de cambio es de: {salario_dolar}")
+
+
