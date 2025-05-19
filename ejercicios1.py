ac4a10d5d473bb709c148ad07f9dff854fdf60e9
valeriat27 <valeriatobon200614@gmail.com>
2025-05-19 07:27:49 -0500

    Add files via upload

---
+++ ejercicios1.py
@@ -0,0 +1,6 @@
+peso_del_payaso = 112
+peso_muñeca = 75
+payasos = int(input("ingrese el numero de payasos a mandar: "))
+muñecas = int(input("ingrese el numero de muñecas a mandar: "))
+peso_total = peso_del_payaso * payasos + peso_muñeca * muñecas
+print("el peso total del paquete es: " +str(peso_total) )
